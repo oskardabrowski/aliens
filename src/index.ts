2c33159faecce0140b56476b00751c7bda9cc83a
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2022-08-03 21:54:32 +0200

    First Commit Rebuild project after git error

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import {CanvasView} from "./view/CanvasView";
+import SHIP_URL from './images/Ship.png';
+
+const canvas:HTMLCanvasElement = document.querySelector('#PlayField');
+const ShipX:number = (canvas.width /2) - (15/2);
+const ShipY:number = (canvas.height /2) - (15/2);
+
+
+const image = new Image();
+image.src = SHIP_URL;
+
+const ShipObj: {img: HTMLImageElement, width:number, height:number, X:number, Y:number} = {
+    img: image,
+    width: 20,
+    height: 20,
+    X: ShipX,
+    Y: ShipY
+}
+
+
+function gameLoop(view: CanvasView):void {
+    view.clear();
+    view.drawShip(ShipObj.img, ShipObj.width, ShipObj.height, ShipObj.X, ShipObj.Y)
+
+    requestAnimationFrame(() => gameLoop(view))
+}
+
+function startGame(view: CanvasView):void {
+    gameLoop(view)
+}
+
+document.addEventListener('keydown', (e) => {
+    if(e.key === 'ArrowUp') ShipObj.Y = ShipObj.Y - 5;
+    if(e.key === 'ArrowDown') ShipObj.Y = ShipObj.Y + 5;
+    if(e.key === 'ArrowLeft') ShipObj.X = ShipObj.X - 5;
+    if(e.key === 'ArrowRight') ShipObj.X = ShipObj.X + 5;
+})
+
+// ArrowUp ArrowDown ArrowLeft ArrowRight
+
+const view = new CanvasView('#PlayField');
+view.initGame(startGame)
+
+
+
+
+
+
+
+
+
---
+++ src/view/CanvasView.ts
@@ -0,0 +1,31 @@
+export class CanvasView {
+    canvas:HTMLCanvasElement;
+    context: CanvasRenderingContext2D;
+    start: HTMLObjectElement;
+
+    constructor(canvasName:string) {
+        this.canvas = document.querySelector(canvasName) as HTMLCanvasElement;
+        this.context = this.canvas.getContext('2d');
+        this.start = document.querySelector('#start')
+    }
+
+    clear() {
+        this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
+    }
+
+    initGame(startFn: (view: CanvasView) => void) {
+        this.start.addEventListener('click', () => startFn(this))
+        console.log('GameInitialized')
+    }
+
+    drawShip(img: HTMLImageElement, Width: number, Height: number, X: number, Y:number) {
+        this.context.drawImage(
+            img,
+            X,
+            Y,
+            Width,
+            Height
+        )
+    }
+
+}
